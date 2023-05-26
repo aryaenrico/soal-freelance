10c1492f755e5c91b5d4fa4540cee8d8eda054aa
Arya Enrico <aryaenrico789@gmail.com>
2023-05-26 15:05:25 +0700

    soal no 1 dan 3

---
+++ index.js
@@ -0,0 +1,7 @@
+const questions = require("./utils");
+
+let soal3 =[2,5,1,12,-5,4,-1,3,-3,20,8,7,-2,6,9];
+let ordered =questions.orderedArray(soal3)
+let soal1 =[2,1,5,7,4,-8,-3,-1];
+console.info(questions.reversedOrdered(ordered));
+console.info(questions.sum0(soal1));
---
+++ utils.js
@@ -0,0 +1,60 @@
+function orderedArray(arr) {
+  let temp;
+  for (i = 0; i < arr.length - 1; i++) {
+    for (j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+function reversedOrdered(arr) {
+  let result = [];
+  let start = 0;
+  let end = arr.length - 1;
+  let flag = false;
+  let counterA = 0;
+  let counterB = 0;
+  for (i = 0; i < arr.length; i++) {
+    if (flag) {
+      result.push(arr[end]);
+      end--;
+      counterB--;
+      if (counterB == 0) {
+        counterA = 0;
+        flag = false;
+      }
+    } else {
+      result.push(arr[start]);
+      counterA++;
+      start++;
+      if (counterA == 5) {
+        counterB = 5;
+        flag = true;
+      }
+    }
+  }
+  return result;
+}
+
+function sum0(arr) {
+  let result = [];
+  let sum = 1;
+  for (i = 0; i < arr.length; i++) {
+    for (j = i; j < arr.length; j++) {
+      sum = arr[i] + arr[i + 1] + arr[j + 2];
+      if (sum === 0) {
+        result.push(arr[i]);
+        result.push(arr[i + 1]);
+        result.push(arr[j + 2]);
+        break;
+      }
+    }
+  }
+  return result;
+}
+module.exports = { orderedArray, reversedOrdered, sum0 };
